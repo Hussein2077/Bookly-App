d201164f4d6ddd6d20063b6e48b4273ce3f4e6f8
hussein <husseinyasser2077@gmail.com>
2023-05-05 19:19:46 +0300

    first commit

---
+++ lib/features/splash/presntation/views/widgets/splash_body.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class SplashBody extends StatelessWidget {
+  const SplashBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
